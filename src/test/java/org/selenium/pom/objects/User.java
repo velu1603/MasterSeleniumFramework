a13caf6fcb9248759e09ca4303d338106a9f2aa0
Velan Chinnaiah <velu1603@gmail.com>
2021-11-25 19:33:07 +0000

    Added tests using API

--- src/test/java/org/selenium/pom/objects/User.java
+++ src/test/java/org/selenium/pom/objects/User.java
@@ -4,8 +4,26 @@ public class User {
 
     private String username;
     private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+
 
     public User(){
+    }
+
+    public User(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
     }
 
     public User(String username, String password){
@@ -16,16 +34,18 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
+    public User setUsername(String username) {
         this.username = username;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public User setPassword(String password) {
         this.password = password;
+        return this;
     }
 
 
